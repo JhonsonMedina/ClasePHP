6c94e987225a6b89b8e93254bfe59f5dc9f47931
Jhonson Medina <jhonsonamedinah@gmail>
2024-08-19 13:32:22 -0400

    terminando practica como administrador y vista de Usuario

--- portafolio/index.php
+++ portafolio/index.php
@@ -1,5 +1,11 @@
 <?php  include("cabecera.php");   ?>
+<?php  include("conexion.php");   ?>
+<?php
 
+$objConexion= new conexion();
+$proyectos=$objConexion->consultar("SELECT * FROM `proyectophp`");
+
+?>
 
 <div class="p-5 mb-4 bg-light rounded-3">
     <div class="container-fluid py-5">
@@ -18,5 +24,23 @@
 
 
 
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+
+<?php  foreach($proyectos as $proyecto) { ?>
+
+  <div class="col">
+    <div class="card">
+      <img src="Imagenes/<?php echo $proyecto['Imagen'];  ?> " class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $proyecto['Nombre'];  ?></h5>
+        <p class="card-text"><?php echo $proyecto['Descripción'];  ?></p>
+      </div>
+    </div>
+    </div>
+    <?php }  ?>
+    
+  </div>
+ 
 
     <?php  include("pie.php");   ?>
--- portafolio/portafolio.php
+++ portafolio/portafolio.php
@@ -1,5 +1,122 @@
 <?php  include("cabecera.php");   ?>
+<?php  include("conexion.php");   ?>
+<?php
 
-    Hola develoteca soy tu portafolio
+
+if($_POST){
+$nombre= $_POST['nombre'];
+$descripcion= $_POST['Descripcion'];
+
+$fecha= new DateTime();
+
+$imagen =$fecha->getTimesTamp()."_".$_FILES['archivo']['name'];
+
+$imagen_temporal= $_FILES['archivo']['tmp_name'];
+
+move_uploaded_file($imagen_temporal,"imagenes/". $imagen);
+
+$objConexion= new conexion();
+$sql="INSERT INTO `proyectophp` (`id`, `Nombre`, `Imagen`, `Descripción`) VALUES (NULL, '$nombre', '$imagen', '$descripcion');";
+$objConexion->ejecutar($sql);
+header("location: portafolio.php");
+
+}
+// "DELETE FROM proyectophp WHERE `proyectophp`.`id` = 3"?
+
+
+if($_GET){
+    $id= $_GET['borrar'];
+    $objConexion = new conexion();
+
+    $imagen=$objConexion->consultar("SELECT imagen FROM `proyectophp` WHERE id=".$id);
+    unlink("imagenes/".$imagen[0]['imagen']); 
+
+    $sql= "DELETE FROM proyectophp WHERE`proyectophp`.`id`=".$id;
+    $objConexion->ejecutar($sql);
+    header("location: portafolio.php");
+
+}
+//Para consultar formulario
+
+$objConexion= new conexion();
+$proyectos=$objConexion->consultar("SELECT * FROM `proyectophp`");
+
+?>
+
+</br>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+
+        <div class="card">
+    <div class="card-header"> Datos del Proyecto</div>
+    <div class="card-body">
+       
+    <form action="portafolio.php" method="post" enctype="multipart/form-data">
+
+Nombre del proyecto: <input required class="form-control" type="text" name="nombre" id="">
+</br>
+Imagen del proyecto: <input required class="form-control" type="file" name="archivo" id="">
+</br>
+Descripcion:
+   <textarea required class="form-control" name="Descripcion" id="" rows="3"></textarea>
+   </br>
+
+   <input class="btn btn-success" type="submit" value="Enviar Proyecto">
+
+   </form>
+
+    </div>
+   
+</div>
+            
+        </div>
+
+        <div class="col-md-6">
+
+        <div class="table-responsive">
+
+<table class="table table-primary">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Imagen</th>
+            <th>Descripción</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php  foreach($proyectos as $proyecto) { ?>
+
+    <tr>
+            <td><?php echo $proyecto['id'];  ?></td>
+            <td><?php echo $proyecto['Nombre'];  ?></td>
+      
+            <td>
+
+        <img width="60" src="Imagenes/<?php echo $proyecto['Imagen']; ?>"mm alt="" srcset="">
+           
+
+            </td>
+
+            <td><?php echo $proyecto['Descripción'];  ?></td>
+            <td> <a class="btn btn-danger" href="?borrar=<?php echo $proyecto['id']; ?>" >Eliminar</a></td>
+         
+        </tr>
+
+        <?php }  ?>
+
+    </tbody>
+</table>
+</div>
+            
+        </div>
+        
+    </div>
+</div>
+
 
 <?php  include("pie.php");   ?>
